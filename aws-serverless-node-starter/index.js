61768a4cd1ded059170cc3febdcfc0c21047a813
Artu <artuon@gmail.com>
2023-07-20 18:30:49 +0300

    env vars

--- aws-serverless-node-starter/index.js
+++ aws-serverless-node-starter/index.js
@@ -5,6 +5,8 @@ module.exports.handler = async (event) => {
       {
         message: 'Go Serverless v3.0! The function executed successfully!',
         input: event,
+        env1: process.env.ENV1,
+        env2: process.env.ENV2,
       },
       null,
       2
